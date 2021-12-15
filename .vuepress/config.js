3ad73e004c5434b327c0ed00f3b6be00631c1a3b
Fabio <fabio.zanchi@ecn.net.au>
2021-12-15 11:19:08 +1000

    starting ember KB

---
+++ .vuepress/config.js
@@ -0,0 +1,39 @@
+const getChildren = require('./childScript')
+
+module.exports = {
+  title: 'Ember  |  Knowledge Base',
+  description: '',
+  plugins: [
+    '@vuepress/back-to-top',
+    // [
+    //   '@vuepress/google-analytics',
+    //   {
+    //     'ga': 'UA-107905087-8' // UA-00000000-0
+    //   }
+    // ],
+  ],
+  head: [
+    ['link', { rel: 'icon', href: '/images/ember_logo.svg' }]
+  ],
+  themeConfig: {
+    logo: '/images/ember_logo.svg',
+    nav: [{
+      text: 'Home',
+      link: '/'
+    },
+    {
+      text: 'Guides',
+      link: '/guides/ember/'
+    },
+    ],
+    sidebar: {
+      '/guides/ember/': [{
+        title: 'Ember',
+        children: getChildren('./guides/ember/')
+      }],
+    },
+    activeHeaderLinks: true,
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated:', // string | boolean
+  },
+};
---
+++ guides/ember/readme.md
@@ -0,0 +1,14 @@
+---
+title: Ember Knowledge Base
+date: 2021-12-15T10:41:12.066Z
+---
+# **Ember Knowledge Base**
+
+- - -
+
+Welcome to Ember Knowledge Base
+
+For more information visit [ember.cadiz3.com.au](https://ember.cadiz3.com.au).
+
+
+
